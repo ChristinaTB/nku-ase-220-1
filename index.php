007abc789dfb41acd3496ac8fb1269202e443a49
Christina <burchfieldchristina@gmail.com>
2021-05-01 17:38:09 -0400

    switch to rock paper scissor

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-$name = "Christina "
-
+$options = ['rock', 'paper', 'scissor'];
+$result = rand(0, count($options)-1);
 ?>
 
 <!doctype html>
@@ -14,10 +14,13 @@ $name = "Christina "
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Rock, paper, or scissor</title>
   </head>
   <body>
-    <h1><?= 'Hello'. $name ?></h1>
+  <div class ="container">
+<h1><?= $result ?></h1>
+  </div>
+    
 
     <!-- Optional JavaScript; choose one of the two! -->
 
